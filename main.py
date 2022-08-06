e8660d945509efc7cc1dfdfa9d96f993112bf0f6
Luis Leonardo <llrwork1@gmail.com>
2022-08-06 16:16:10 -0500

    Create database_connection function

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from tools import database_library
 
 def main():
-
     db_name = input("What the database should be named?")
-    database_library.create_connection(db_name)
+    database_library.create_connection()
 
 if __name__ == "__main__":
     main()
-
-
--- tools/database_library.py
+++ tools/database_library.py
@@ -1,6 +1,9 @@
 import sqlite3
 
-def create_connection(db_file):
-    sqliteConnection = sqlite3.connect(db_file)
+def create_connection():
+    sqlite_connection = sqlite3.connect("database.db")
+    cursor = sqlite_connection.cursor()
     print("DB Init")
+
+#def make_tables(table_name):
 
